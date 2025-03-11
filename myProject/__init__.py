44333f1ae5910e2fbe72acd1c9576e5e1e0448a7
hailey-sar <haileysarmiento@g.ucla.edu>
2025-03-11 12:19:39 -0700

    to import all functions from myProject

--- myProject/__init__.py
+++ myProject/__init__.py
@@ -14,6 +14,11 @@ from .API_calls import (fetch_nasa_data, get_nasa_power_hourly_data,
 from .weather_visualization import (plot_hourly, plot_yearly,
                                    plot_mock_heatmap)
 
+# Import functions from keras_weather_model.py
+from .keras_weather_model import (run_model, plot_predictions)
+
+# Import functions from app.py
+from .app import (load_app, choose_graph)
 
 # In[ ]:
 
